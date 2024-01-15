6e3d4582728f68355a13e690cfc141f3ddbb98b2
giang8692@gmail.com <giang8692@gmail.com>
2024-01-16 00:39:44 +0700

    Add project files.

---
+++ WebAPI/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    [Table("Category")]   
+    
+    public class Category
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string CategoryAlias { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+        [Required(ErrorMessage = "Category Name is required")]
+        [StringLength(100, ErrorMessage = "Category Name cannot be longer than 100 characters")]
+        public string CategoryDescription { get; set; }
+
+        public List<Product> Products { get; set; } 
+    }
+}
---
+++ WebAPI/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string ProductAlias { get; set; }
+        [Required]
+
+        public string SKU { get; set; }
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(100, ErrorMessage = "Product Name cannot be longer than 100 characters")]
+
+        public string ProductDescription { get; set; }
+
+        
+        public string ProductPrice { get; set; }
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+
+        public string ProductImage { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
